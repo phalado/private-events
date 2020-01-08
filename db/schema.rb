1b8a6534b8ac015ff0a775a73caf9f192a7f2831
alejandro-andres <alejandro-andres@debian.alex>
2020-01-08 17:51:07 -0300

    added event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_175610) do
+ActiveRecord::Schema.define(version: 2020_01_08_202405) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "events", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.string "password"
-    t.string "password_confimartion"
+    t.string "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
