e5e6bdee07e61073af451b7cff8457edf3a7b740
alejandro-andres <alejandro-andres@debian.alex>
2020-01-10 17:42:34 -0300

    created scopes and views for past and upcoming events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,5 +17,7 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
+    @past_events = Event.past
+    @upcoming_events = Event.upcoming
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user, foreign_key: "creator"
   has_many :attendance, foreign_key: "attended_event"
   has_many :attendants, through: :attendance, source: :attendee
+  
+  scope :past, -> {where('date < ?', current_time = DateTime.now)}
+  scope :upcoming, -> {where('date >= ?', current_time = DateTime.now)}
 end
